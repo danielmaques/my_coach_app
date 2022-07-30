e8ca5b8fd1983b6ac2ae7c8cabedd5bc30abf099
ddanielmarques <daniel.marques@sciensa.com>
2022-07-30 16:06:54 -0300

    tela de login

--- lib/modules/login/login_module.dart
+++ lib/modules/login/login_module.dart
@@ -1,26 +1,17 @@
-import 'package:flutter/cupertino.dart';
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:my_coach_app/modules/app/data/datasources/user_datasource_impl.dart';
 import 'package:my_coach_app/modules/login/ui/login_page.dart';
-import 'package:my_coach_app/shared/services/auth_service.dart';
 
-import 'domain/usecases/user_usecase.dart';
-
-class AppModule extends Module {
+class LoginModule extends Module {
   @override
   List<Module> get imports => [];
 
   @override
-  List<Bind> get binds => [
-        // User
-        Bind.factory((i) => UserDatasourceImpl()),
-        Bind.factory((i) => UserUseCaseImpl(i())),
-      ];
+  List<Bind> get binds => [];
 
   @override
   List<ModularRoute> get routes => [
-        ChildRoute('/',
-            child: (context, args) =>
-                AuthServices().usuario == null ? LoginPage() : Container()),
+        ChildRoute('/', child: (context, args) => LoginPage()),
       ];
 }
--- lib/modules/login/ui/login_page.dart
+++ lib/modules/login/ui/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_coach_ui/my_coach_ui.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -8,8 +9,45 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  late TextEditingController emailController;
+  late TextEditingController senhaController;
+
+  @override
+  void initState() {
+    emailController = TextEditingController();
+    senhaController = TextEditingController();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: AppColors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+            const Spacer(),
+            CoachInputField(
+              controller: emailController,
+              label: 'Email',
+              onChanged: (value) {},
+            ),
+            const SizedBox(height: 12),
+            CoachInputField(
+              controller: senhaController,
+              prefixIcon: AppIcons.lock_closed,
+              label: 'Senha',
+              onChanged: (value) {},
+            ),
+            const SizedBox(height: 48),
+            const CoachButton(
+              buttonRadius: true,
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
   }
 }
